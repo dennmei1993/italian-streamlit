33fdf50c0b190dd9b06397b05715c56b41e39411
dennmei1993 <dennmei1993@gmail.com>
2026-01-17 20:29:09 +1100

    Add files via upload

--- app.py
+++ app.py
@@ -689,7 +689,7 @@ st.markdown(
         position: fixed;
         left: 0;
         right: 0;
-        top: 70vh;
+        top: 80vh;
         height: 40vh;
         max-width: 100% !important;
         margin: 0 !important;
